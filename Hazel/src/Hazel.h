ed67c6ba851ea05574968d0dad1aa1ab7f1c167c
Liuzilong <lzl.scut@outlook.com>
2020-06-12 16:22:05 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <cstdio>
+//For use by Hazel applications
+
+#include "Hazel/Application.h"
+
+
+//---EntryPoint----------------------
+#include "Hazel/EntryPoint.h"
+//-----------------------------------
+
---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,20 @@
+#include "Application.h"
+
+namespace Hazel
+{
+	void Application::Run()
+	{
+		while (true);
+	}
+	Application::Application()
+	{
+		
+	}
+	Application::~Application()
+	{
+		
+	}
+
+	//To be define in CLIENT
+
+}
